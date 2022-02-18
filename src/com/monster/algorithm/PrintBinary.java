585eea6392dba29a2f9125da647943fb0feff8e6
monster <12@qq.com>
2022-02-18 16:50:36 +0800

    新增位运算符

--- src/com/monster/algorithm/PrintBinary.java
+++ src/com/monster/algorithm/PrintBinary.java
@@ -33,12 +33,41 @@ public class PrintBinary {
 
 
         //移位
-        int test = 1;
+//        int test = 1;
         //00000000000000000000000000000001
-        printBinary(test);
+//        printBinary(test);
         //左移一位 00000000000000000000000000000010
-        printBinary(test << 1);
+//        printBinary(test << 1);
         //左移八位 00000000000000000000000100000000
-        printBinary(test << 8);
+//        printBinary(test << 8);
+
+        //打印整型最大值
+//        int max = Integer.MAX_VALUE;
+//        printBinary(max);
+
+        //打印整型最小值
+//        int min = Integer.MIN_VALUE;
+//        printBinary(min);
+
+        /**
+         * 位运算概述：
+         *  & ： 与  两个位都为 1 时，结果才为 1；
+         *  | ： 或  两个位都为 0 时。结果才为 0；
+         *  ^ ： 异或 两个位相同是为 0，相异为 1；
+         *  ~ ： 取反  0 变 1, 1 变 0；
+         *  << : 左移 各二进制全部左移若干位，高位丢弃，低位补 0
+         *  >> ： 右移 各二进位全部右移若干位，对无符号数，高位补0，有符号数，各编译器处理方法不一样，有的补符号位（算术右移），有的补0（逻辑右移）
+         */
+        int a = 12358742;
+        int b = 5485244;
+        printBinary(a);
+        printBinary(~a);
+        printBinary(b);
+        System.out.println("====================");
+        printBinary(a | b);
+        printBinary(a & b);
+        printBinary(a ^ b);
+
+
     }
 }
